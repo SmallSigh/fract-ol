ac731bd5b86e434596e30d338801d836c412d647
SmallSigh <martijnsmit4@gmail.com>
2025-03-27 14:50:56 +0100

    fixed segfault on fullscreen

--- src/mlx_init.c
+++ src/mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 17:13:05 by masmit            #+#    #+#             */
-/*   Updated: 2025/03/27 14:32:05 by masmit           ###   ########.fr       */
+/*   Updated: 2025/03/27 14:49:29 by masmit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,31 @@ void	choose_window_size(fractal_t *f)
 	int32_t	width;
 	int32_t	height;
 
+	f->w_size.height = HEIGHT;
+	f->w_size.width = WIDTH;
+	f->mlx = mlx_init(WIDTH, HEIGHT, "small fractol", false);
 	if (f->flag.maximize)
 	{
 		mlx_get_monitor_size(0, &width, &height);
-		ft_printf("width = %i\n", width);
-		ft_printf("heigth = %i\n", height);
+		mlx_set_window_size(f->mlx, width, height);
 		f->w_size.height = height;
 		f->w_size.width = width;
+		mlx_terminate(f->mlx);
 		f->mlx = mlx_init(width, height, "Big Fractol", true);
-	}
-	else
-	{
-		f->w_size.height = HEIGHT;
-		f->w_size.width = WIDTH;
-		f->mlx = mlx_init(WIDTH, HEIGHT, "small fractol", false);
 	}
 }
 
 void	start_mlx(fractal_t *f)
 {
 	choose_window_size(f);
-	ft_printf("no segfault here\n");
 	mlx_key_hook(f->mlx, ft_hook, f);
 	mlx_scroll_hook(f->mlx, ft_scrollhook, f);
-	ft_printf("segfault here\n");
 }
 
 void	cleanup(fractal_t *f)
 {
 	if (f->img)
 		mlx_delete_image(f->mlx, f->img);
-	if (f->mlx != NULL)
+	if (f->mlx)
 		mlx_terminate(f->mlx);
 }
